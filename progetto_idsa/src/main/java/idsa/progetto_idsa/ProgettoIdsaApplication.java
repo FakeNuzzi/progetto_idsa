88bacd5a3b9d5c5a8c9392e2461ae246f55e29b5
federicosavonuzzi <savonuzzi02@gmail.com>
2023-10-25 00:03:17 +0200

    configuration

---
+++ progetto_idsa/src/main/java/idsa/progetto_idsa/ProgettoIdsaApplication.java
@@ -0,0 +1,13 @@
+package idsa.progetto_idsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoIdsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoIdsaApplication.class, args);
+	}
+
+}
