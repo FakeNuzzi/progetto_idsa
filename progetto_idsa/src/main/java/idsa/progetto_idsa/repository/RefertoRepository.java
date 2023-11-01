5c7f44dec7de97c4c7a766d83de2d050fe7cff74
FraResca <francesco.resca@edu.unife.it>
2023-11-01 16:06:02 +0100

    mini fix senza risultato

--- progetto_idsa/src/main/java/idsa/progetto_idsa/repository/RefertoRepository.java
+++ progetto_idsa/src/main/java/idsa/progetto_idsa/repository/RefertoRepository.java
@@ -1,7 +1,8 @@
 package idsa.progetto_idsa.repository;
 
 import idsa.progetto_idsa.entity.Referto;
+import idsa.progetto_idsa.entityID.RefertoID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RefertoRepository extends JpaRepository<Referto,Long> {
+public interface RefertoRepository extends JpaRepository<Referto,RefertoID> {
 }
--- progetto_idsa/src/main/java/idsa/progetto_idsa/repository/TicketRepository.java
+++ progetto_idsa/src/main/java/idsa/progetto_idsa/repository/TicketRepository.java
@@ -1,7 +1,8 @@
 package idsa.progetto_idsa.repository;
 
 import idsa.progetto_idsa.entity.Ticket;
+import idsa.progetto_idsa.entityID.TicketID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TicketRepository extends JpaRepository<Ticket,Long> {
+public interface TicketRepository extends JpaRepository<Ticket,TicketID> {
 }
