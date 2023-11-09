49f5f98298eda1a6d57fe321d37dda52fcafe2f7
FraResca <francesco.resca@edu.unife.it>
2023-11-10 00:25:56 +0100

    cross origin paziente per prova react

--- progetto_idsa/src/main/java/idsa/progetto_idsa/controller/PazienteController.java
+++ progetto_idsa/src/main/java/idsa/progetto_idsa/controller/PazienteController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/paziente")
 @AllArgsConstructor
