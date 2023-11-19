92d337ba4f5bf612467414450ca50659f18e15b9
FraResca <francesco.resca@edu.unife.it>
2023-11-19 23:19:45 +0100

    correzione Cors

--- progetto_idsa/src/main/java/idsa/progetto_idsa/controller/RefertoController.java
+++ progetto_idsa/src/main/java/idsa/progetto_idsa/controller/RefertoController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/referto")
+@CrossOrigin("*")
+@RequestMapping("/api/referti")
 @AllArgsConstructor
 public class RefertoController {
 
